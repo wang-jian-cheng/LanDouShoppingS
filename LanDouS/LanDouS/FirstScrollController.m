646ccadef461ab9565c50d2a4e0bc39a8bfb50cb
王建成 <w754550398@qq.com>
2016-02-19 14:42:34 +0800

    更换图片

--- LanDouS/LanDouS/FirstScrollController.m
+++ LanDouS/LanDouS/FirstScrollController.m
@@ -8,8 +8,13 @@
 
 #import "FirstScrollController.h"
 
-@interface FirstScrollController ()
+#define NUMBER_OF_PAGES     5
+#define PAGECTL_ON_IMG      1
 
+@interface FirstScrollController ()
+{
+    UIButton *nextBtn;
+}
 @end
 
 @implementation FirstScrollController
@@ -19,37 +24,62 @@
     scrollBG=[[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
     scrollBG.delegate=self;
     scrollBG.showsHorizontalScrollIndicator=NO;
-    scrollBG.contentSize=CGSizeMake(SCREEN_WIDTH*3, SCREEN_HEIGHT);
+    scrollBG.contentSize=CGSizeMake(SCREEN_WIDTH*NUMBER_OF_PAGES, SCREEN_HEIGHT);
     scrollBG.pagingEnabled=YES;
     [self.view addSubview:scrollBG];
     
-    for (int i=0; i<3; i++) {
+    for (int i=0; i<NUMBER_OF_PAGES; i++) {
         UIImageView *imgInfo=[[UIImageView alloc]initWithFrame:CGRectMake(SCREEN_WIDTH*i,0,SCREEN_WIDTH,SCREEN_HEIGHT)];
         imgInfo.image=[UIImage imageNamed:[NSString stringWithFormat:@"%d.png",i+1]];
         [scrollBG addSubview:imgInfo];
+        
     }
-    
+    nextBtn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 160, 60)];
+    nextBtn.center = CGPointMake(SCREEN_WIDTH/2, SCREEN_HEIGHT/100 *90);
+    [nextBtn addTarget:self action:@selector(changeRootViewBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+#if !PAGECTL_ON_IMG
     pageCol=[[UIPageControl alloc]initWithFrame:CGRectMake(0, SCREEN_HEIGHT-30, 320, 20)];
-    pageCol.numberOfPages=3;
+    pageCol.numberOfPages=NUMBER_OF_PAGES;
     //pageCol.center=CGPointMake(SCREEN_WIDTH/2, SCREEN_HEIGHT-30);
     [self.view addSubview:pageCol];
-    
+#endif
     
     // Do any additional setup after loading the view from its nib.
 }
 - (void)scrollViewDidScroll:(UIScrollView *)sender
 {
     
+    
+#if !PAGECTL_ON_IMG
     int pageIndex = fabs(sender.contentOffset.x) /sender.frame.size.width;
     pageCol.currentPage = pageIndex;
+#endif
     
-    if (sender.contentOffset.x>(SCREEN_WIDTH*2+40)) {
+
+    if(sender.contentOffset.x>=(SCREEN_WIDTH*(NUMBER_OF_PAGES - 2)+40))
+    {
+        
+        [self.view addSubview:nextBtn];
+    }
+    else
+    {
+        [nextBtn removeFromSuperview];
+    }
+    
+    
+    if (sender.contentOffset.x>(SCREEN_WIDTH*(NUMBER_OF_PAGES - 1)+40)) {
         
         set_sp(@"FIRST_ENTER", @"1");
         [[NSNotificationCenter defaultCenter] postNotificationName:@"changeRootView" object:nil ];
     }
     
     
+}
+
+-(void)changeRootViewBtnClick:(UIButton *)sender
+{
+    set_sp(@"FIRST_ENTER", @"1");
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"changeRootView" object:nil ];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
